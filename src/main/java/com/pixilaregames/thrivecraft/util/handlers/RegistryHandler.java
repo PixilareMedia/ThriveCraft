6ea275e8e584f01bbac357a780e62713d50bebe3
HerobrainGamer <47619029+HerobrainGamer@users.noreply.github.com>
2019-02-13 22:53:40 -0800

    Create RegistryHandler.java

---
+++ src/main/java/com/pixilaregames/thrivecraft/util/handlers/RegistryHandler.java
@@ -0,0 +1,32 @@
+package com.pixilaregames.thrivecraft.util.handlers;
+
+import com.pixilaregames.thrivecraft.init.ModItems;
+import com.pixilaregames.thrivecraft.util.IHasModel;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@EventBusSubscriber
+public class RegistryHandler {
+	
+	@SubscribeEvent
+	public static void onItemRegister(RegistryEvent.Register<Item> event)
+	{
+		event.getRegistry().registerAll(ModItems.ITEMS.toArray(new Item[0]));
+	}
+
+	@SubscribeEvent
+	public static void onModelRegister(ModelRegistryEvent event)
+	{
+		for(Item item : ModItems.ITEMS)
+		{
+			if(item instanceof IHasModel)
+			{
+				((IHasModel)item).registerModels();
+			}
+		}
+	}
+}
