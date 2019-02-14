91dbead8bac5c1da1b1189ff244ad04f85997c32
HerobrainGamer <47619029+HerobrainGamer@users.noreply.github.com>
2019-02-13 22:52:17 -0800

    Create CommonProxy.java

---
+++ src/main/java/com/pixilaregames/thrivecraft/proxy/CommonProxy.java
@@ -0,0 +1,12 @@
+package com.pixilaregames.thrivecraft.proxy;
+
+import net.minecraft.item.Item;
+
+public class CommonProxy {
+
+	public void registerItemRenderer(Item item, int meta, String id)
+	{
+		
+	}
+	
+}
