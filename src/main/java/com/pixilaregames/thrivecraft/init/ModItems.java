aa3c95b136d14d38f47372fd46ece2b11cde5de7
HerobrainGamer <47619029+HerobrainGamer@users.noreply.github.com>
2019-02-13 22:50:59 -0800

    Create ModItems.java

---
+++ src/main/java/com/pixilaregames/thrivecraft/init/ModItems.java
@@ -0,0 +1,17 @@
+package com.pixilaregames.thrivecraft.init;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.pixilaregames.thrivecraft.items.ItemBase;
+
+import net.minecraft.item.Item;
+
+public class ModItems {
+	
+	public static final List<Item> ITEMS = new ArrayList<Item>();
+	
+	public static final Item PETRIFIED_SAPLING = new ItemBase("petrified_sapling");
+	public static final Item PETRIFIED_DOOR = new ItemBase("petrified_door");
+
+}
