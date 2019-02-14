292aee4202018effe1e6453fed4cae0e616772a5
HerobrainGamer <47619029+HerobrainGamer@users.noreply.github.com>
2019-02-14 07:26:59 -0800

    Create BlockBase.java

---
+++ src/main/java/com/pixilaregames/thrivecraft/blocks/BlockBase.java
@@ -0,0 +1,35 @@
+package com.pixilaregames.thrivecraft.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+
+import com.pixilaregames.thrivecraft.Main;
+import com.pixilaregames.thrivecraft.init.ModBlocks;
+import com.pixilaregames.thrivecraft.init.ModItems;
+import com.pixilaregames.thrivecraft.util.IHasModel;
+
+public class BlockBase extends Block implements IHasModel
+{
+
+	public BlockBase(String name, Material material)
+	{
+		super(material);
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+		
+		ModBlocks.BLOCKS.add(this);
+		ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+	}
+
+	@Override
+	public void registerModels() {
+		
+		Main.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
+		
+	}
+	
+}
