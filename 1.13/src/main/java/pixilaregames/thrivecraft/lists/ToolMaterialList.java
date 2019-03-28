495e6c1fc0133905fd03ba3562a2211a7070fb78
HerobrainGamer <herobraingaming@gmail.com>
2019-03-28 11:48:48 -0700

    code
    change some values

--- 1.13/src/main/java/pixilaregames/thrivecraft/lists/ToolMaterialList.java
+++ 1.13/src/main/java/pixilaregames/thrivecraft/lists/ToolMaterialList.java
@@ -7,7 +7,7 @@ import net.minecraft.item.crafting.Ingredient;
 public enum ToolMaterialList implements IItemTier
 {
 	onyx(4.0f, 9.0f, 1942, 4, 10, ItemList.onyx_gem),
-	toothy(4.0f, 9.0f, 1942, 4, 10, ItemList.dracula_fang);
+	toothy(6.0f, 2.0f, 250, 2, 14, ItemList.dracula_fang);
 	
 	private float attackDamage, efficiency;
 	private int durablity, harvestLevel, enchantablity;
